0e32fc418a6cf180ef6d8efbec401956753253e4
vickramravichandran <vickram@Vickrams-MacBook-Air.local>
2017-02-25 21:27:54 -0500

    Initial

---
+++ scripts/app.js
@@ -0,0 +1,162 @@
+if (!String.prototype.startsWith) {
+    String.prototype.startsWith = function (searchString, position) {
+        position = position || 0;
+        return this.substr(position, searchString.length) === searchString;
+    };
+}
+
+(function () {
+    'use strict';
+
+    angular
+        .module('mainApp', ['autoCompleteModule'])
+        .controller('SimpleListCtrl', SimpleListCtrl)
+        .controller('CustomListUsingTemplateCtrl', CustomListUsingTemplateCtrl)
+        .controller('SimpleListRemoteDataCtrl', SimpleListRemoteDataCtrl)
+        .controller('CustomListUsingTemplateUrlCtrl', CustomListUsingTemplateUrlCtrl)
+        .controller('RemoteDataUsingRenderItemCtrl', RemoteDataUsingRenderItemCtrl)
+        .controller('PluginOptionsCtrl', PluginOptionsCtrl)
+        .directive('ngPrism', function() {
+            return {
+                restrict: 'A',
+                link: function (scope, element, attrs) {
+                    element.ready(function () {
+                        Prism.highlightElement(element[0]);
+                    });
+                }
+            }
+        });
+
+    // upper case color code and name
+    _.each(CSS_COLORS, function (value) {
+        value.code = value.code.toUpperCase();
+        value.name = value.name.toUpperCase();
+    });
+
+    function SimpleListCtrl() {
+        var that = this;
+        that.colorName = null;
+
+        that.autoCompleteOptions = {
+            minimumChars: 1,
+            data: function (term) {
+                term = term.toUpperCase();
+                var match = _.filter(CSS_COLORS, function (value) {
+                    return value.name.startsWith(term);
+                });
+                return _.pluck(match, 'name');
+            }
+        }
+    }
+
+    // Using itemTemplate
+    CustomListUsingTemplateCtrl.$inject = ["$templateCache"];
+    function CustomListUsingTemplateCtrl($templateCache) {
+        var that = this;
+        that.colorName = '';
+        that.selectedColor = null;
+
+        that.autoCompleteOptions = {
+            minimumChars: 1,
+            data: function (term) {
+                term = term.toUpperCase();
+                return _.filter(CSS_COLORS, function (value) {
+                    return value.name.startsWith(term);
+                });
+            },
+            dropdownWidth: '400px',
+            containerCssClass: 'color-codes',
+            selectedTextAttr: 'name',
+            itemTemplate: $templateCache.get('color-item-template'),
+            itemSelected: function (e) {
+                that.selectedColor = e.item;
+            }
+        }
+    }
+
+    SimpleListRemoteDataCtrl.$inject = ["$q", "$http"];
+    function SimpleListRemoteDataCtrl($q, $http) {
+        var that = this;
+        that.stateName = null;
+
+        that.autoCompleteOptions = {
+            minimumChars: 1,
+            data: function (term) {
+                return $http.get('data_files/usa_states.json')
+                    .then(function (response) {
+                        // ideally filtering should be done on server
+                        term = term.toUpperCase();
+                        var match = _.filter(response.data, function (value) {
+                            return value.name.startsWith(term);
+                        });
+                        return _.pluck(match, 'name');
+                    });
+            }
+        }
+    }
+
+    // Using itemTemplateUrl
+    function CustomListUsingTemplateUrlCtrl() {
+        var that = this;
+        that.colorName = '';
+        that.selectedColor = null;
+
+        that.autoCompleteOptions = {
+            minimumChars: 1,
+            data: function (term) {
+                if (!term.startsWith('#')) {
+                    term = '#' + term;
+                }
+                term = term.toUpperCase();
+                return _.filter(CSS_COLORS, function (value) {
+                    return value.code.startsWith(term);
+                });
+            },
+            containerCssClass: 'color-codes',
+            selectedTextAttr: 'code',
+            itemTemplateUrl: 'templates/color-list-item.tpl.html',
+            itemSelected: function (e) {
+                that.selectedColor = e.item;
+            }
+        }
+    }
+
+    // Using renderItem Callback
+    RemoteDataUsingRenderItemCtrl.$inject = ["$q", "$http", "$sce"];
+    function RemoteDataUsingRenderItemCtrl($q, $http, $sce) {
+        var that = this;
+        that.airport = null;
+
+        that.autoCompleteOptions = {
+            minimumChars: 1,
+            dropdownWidth: '500px',
+            dropdownHeight: '200px',
+            data: function (term) {
+                return $http.get('data_files/airports.json')
+                    .then(function (response) {
+                        // ideally filtering should be done on the server
+                        term = term.toUpperCase();
+                        return _.filter(response.data, function (val) {
+                            return val.iata == term
+                                || val.name.startsWith(term);
+                        });
+                    });
+            },
+            renderItem: function (item) {
+                return {
+                    value: item.name,
+                    label: $sce.trustAsHtml("<p class='auto-complete'>" + item.name + "</p>")
+                };
+            },
+            itemSelected: function (e) {
+                that.airport = e.item;
+            }
+        }
+    }
+
+    PluginOptionsCtrl.$inject = ["autoCompleteService"];
+    function PluginOptionsCtrl(autoCompleteService) {
+        this.options = autoCompleteService.defaultOptionsDoc();
+    }
+        
+})();
