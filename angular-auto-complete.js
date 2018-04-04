c451d6689010145b8dd0e395da020e6f5b0c9629
Vickram Ravichandran <vickramganesh@outlook.com>
2018-04-04 08:00:17 -0400

    use parentNode instead of parentElement

--- angular-auto-complete.js
+++ angular-auto-complete.js
@@ -874,7 +874,7 @@
 
                 linkFn(createEntryScope(scope), function (clonedElement) {
                     // append to the directive element's parent (<li>) since this directive element is replaced (transclude is set to 'element').
-                    $(element[0].parentElement).append(clonedElement);
+                    $(element[0].parentNode).append(clonedElement);
                 });
             }
         };
@@ -921,7 +921,7 @@
                 var linkFn = $compile(scope.ctrl.template);
                 linkFn(createEntryScope(scope), function (clonedElement) {
                     // append to the directive element's parent (<li>) since this directive element is replaced (transclude is set to 'element').
-                    $(element[0].parentElement).append(clonedElement);
+                    $(element[0].parentNode).append(clonedElement);
                 });
             }
         };
